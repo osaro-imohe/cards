212f61a50288073aa7200c7ef7baff1f263069d0
Osaro Imohe <osaroimohe99@yahoo.com>
2022-04-04 13:43:42 +0100

    [Client] Update client package.json start script

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,1 +1,1 @@
-export const BASE_URL: string = 'http://localhost:8080';
+export const BASE_URL: string = process.env.REACT_APP_ENVIRONMENT === 'development' ? 'http://localhost:8080' : 'https://flashcard-app-server.herokuapp.com/';
