0751f3b2e77c9ab70ed2649c599eed5ad909617d
Osaro Imohe <osaroimohe99@yahoo.com>
2022-04-04 15:18:16 +0100

    [Server] Udpate start script in package.json

--- server/src/app.ts
+++ server/src/app.ts
@@ -32,8 +32,8 @@ Routes.forEach((route: any) => {
 const run = async () => {
   try {
     syncDb();
-    app.listen(process.env.PORT ?? 8080, () => {
-      console.log(`Application is running on ${process.env.PORT ?? 8080}`);
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Application is running on ${process.env.PORT || 8080}`);
     });
   } catch (e) {
     console.error(`An error occurred: ${JSON.stringify(e)}`);
